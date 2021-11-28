d938bf80f115b2d4b0b445de2692143830ae2e66
Madhu <enometh@net.meer>
2021-11-28 07:17:16 +0530

    tools: don't include asdf and defsystem in the build

--- lib/systems.lisp
+++ lib/systems.lisp
@@ -231,8 +231,8 @@
     (remote-lisp      "ccl:bin;remote-lisp"      ("ccl:library;remote-lisp.lisp" "ccl:lib;swink.lisp"))
  
     (prepare-mcl-environment "ccl:bin;prepare-mcl-environment" ("ccl:lib;prepare-mcl-environment.lisp"))
-    (defsystem        "ccl:tools;defsystem"      ("ccl:tools;defsystem.lisp"))
-    (asdf             "ccl:tools;asdf"	    ("ccl:tools;asdf.lisp"))
+ ;   (defsystem        "ccl:tools;defsystem"      ("ccl:tools;defsystem.lisp"))
+ ;   (asdf             "ccl:tools;asdf"	    ("ccl:tools;asdf.lisp"))
     (jp-encode        "ccl:bin;jp-encode"        ("ccl:library;jp-encode.lisp"))
     (cn-encode        "ccl:bin;cn-encode"        ("ccl:library;cn-encode.lisp"))
     (hashenv          "ccl:bin;hashenv"          ("ccl:xdump;hashenv.lisp"))))
